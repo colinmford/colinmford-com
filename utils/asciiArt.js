31cf2e294256adab2e33d6bc67d46654a18e0115
Colin M. Ford <colinmford@users.noreply.github.com>
2021-08-31 00:40:51 -0400

    Worked out grid overlay, making responsive ascii portrait

--- utils/asciiArt.js
+++ utils/asciiArt.js
@@ -1,13 +1,19 @@
 const imageToAscii = require("image-to-ascii");
 
 // Passing options
-imageToAscii("/Users/colinmford/Downloads/IMG_0499.jpg", {
-    colored: false,
-    preserve_aspect_ratio: true,
-    size: {
-      height: "100%"
-    },
-    pixels: " .:*-+#+−×÷=≠≈<>&%@░▒▓"
-}, (err, converted) => {
-    console.log(err || converted);
-});
+let min = 18;
+let max = 30;
+let imgs = [];
+
+for (let i = max; i >= min; i--) {
+  imageToAscii("/Users/colinmford/Downloads/IMG_0499.jpg", {
+      colored: false,
+      preserve_aspect_ratio: true,
+      size: {
+        width: i
+      },
+      pixels: " .:*-+#+−×÷=&%@░▒▓"
+  }, (err, converted) => {
+      console.log(converted)
+  });
+}
